bc64e7127dde70421e5c785b017e812946816a33
Takayuki Matsumoto <matsu5012@gmail.com>
2026-07-27 19:01:05 +0900

    スキャン画面を追加

--- scanner.js
+++ scanner.js
@@ -1,27 +1,18 @@
-function startScanner() {
+const scanner = new Html5Qrcode("reader");
 
-    const scanner = new Html5Qrcode("reader");
+scanner.start(
+    { facingMode: "environment" },
+    {
+        fps: 10,
+        qrbox: 250
+    },
+    function(decodedText){
 
-    scanner.start(
+        scanner.stop().then(() => {
+            location.href =
+                "book.html?isbn=" + decodedText;
+        });
 
-        { facingMode: "environment" },
-
-        {
-            fps: 10,
-            qrbox: 250
-        },
-
-        function(decodedText){
-
-            scanner.stop();
-
-            window.location.href =
-            "book.html?isbn="+decodedText;
-
-        },
-
-        function(){}
-
-    );
-
-}
+    },
+    function(error){}
+);
