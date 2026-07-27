053687c26d0c527a2730ea578b18f51ed52957c7
Takayuki Matsumoto <matsu5012@gmail.com>
2026-07-27 19:15:23 +0900

    スキャナ起動方法を改善

--- scanner.js
+++ scanner.js
@@ -1,34 +1,33 @@
-window.onload = async function () {
+let scanner;
 
-    const cameras = await Html5Qrcode.getCameras();
+function startScanner() {
 
-    if (cameras && cameras.length > 0) {
-
-        let cameraId = cameras[cameras.length - 1].id;
-
-        const scanner = new Html5Qrcode("reader");
-
-        scanner.start(
-            cameraId,
-            {
-                fps: 10,
-                qrbox: 250
-            },
-            function(decodedText){
-
-                scanner.stop().then(() => {
-                    location.href =
-                        "book.html?isbn=" + decodedText;
-                });
-
-            },
-            function(error){}
-        );
-
-    } else {
-
-        alert("カメラが見つかりません。");
-
+    if (scanner) {
+        return;
     }
 
-};
+    scanner = new Html5Qrcode("reader");
+
+    scanner.start(
+        { facingMode: "environment" },
+        {
+            fps: 10,
+            qrbox: 250
+        },
+        function(decodedText){
+
+            scanner.stop().then(() => {
+                location.href = "book.html?isbn=" + decodedText;
+            });
+
+        },
+        function(error){}
+    ).catch(err => {
+
+        alert("カメラを起動できませんでした。\n" + err);
+
+        scanner = null;
+
+    });
+
+}
